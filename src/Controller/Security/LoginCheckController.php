6d27bd92f98f8a2378db0b180f4c079f8a973a9b
Julien HUMBERT <julien.humbert-prestataire@laposte.fr>
2023-11-02 23:42:47 +0100

    Login check never return type

--- src/Controller/Security/LoginCheckController.php
+++ src/Controller/Security/LoginCheckController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class LoginCheckController extends AbstractController
 {
     #[Route(path: '/login/check', name: 'security_login_check')]
-    public function __invoke(): void
+    public function __invoke(): never
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
--- src/Controller/Security/LoginGithubCheckController.php
+++ src/Controller/Security/LoginGithubCheckController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class LoginGithubCheckController extends AbstractController
 {
     #[Route(path: '/login/check-github', name: 'security_login_github_check')]
-    public function __invoke(): void
+    public function __invoke(): never
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
