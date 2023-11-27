ff14c72b477ad2a108f5fbebd2759aa32a12522b
Julien HUMBERT <julien.humbert-prestataire@laposte.fr>
2023-11-27 14:41:56 +0100

    Search button aria-label

--- assets/vue/components/SearchButton/SearchButton.vue
+++ assets/vue/components/SearchButton/SearchButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="onClick" id="header_search_button" :title="title" :aria-expanded="isSearchOpen ? 'true' : 'false'" aria-controls="#search-form">
+  <button @click="onClick" id="header_search_button" :aria-label="title" :aria-expanded="isSearchOpen ? 'true' : 'false'" aria-controls="#search-form">
     <i class="fa-solid fa-magnifying-glass fa-lg"></i>
   </button>
 </template>
