4cee062ae323b22759b2d5cf157240a44ed7beaf
Julien HUMBERT <julien.humbert-prestataire@laposte.fr>
2024-03-03 13:53:54 +0100

    Editions rss feed

---
+++ src/Controller/Rss/EditionsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Rss;
+
+use App\Client\ApiClient;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EditionsController extends AbstractController
+{
+    #[Route('/rss/editions', name: 'rss_editions')]
+    public function __invoke(ApiClient $apiClient)
+    {
+        $editions = $apiClient->getLatestEditions(limit: 30);
+
+        return $this->render('rss/editions.xml.twig', [
+            'editions' => $editions,
+        ]);
+    }
+}
